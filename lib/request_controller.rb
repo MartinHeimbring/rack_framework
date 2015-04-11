516167df41eb6cbd793bdc5408a47e09f16468bf
Martin Heimbring <martin.heimbring@gmail.com>
2015-04-11 12:48:23 +0200

    adds request controller
    the central rack app object

---
+++ lib/request_controller.rb
@@ -0,0 +1,11 @@
+class RequestController
+  def call(env)
+    route = MainApplication.router.route_for(env["PATH_INFO"], env["REQUEST_METHOD"])
+    if route
+      response = route.execute(env)
+      return response.rack_response
+    else
+      return [404, {}, []]
+    end
+  end
+end
